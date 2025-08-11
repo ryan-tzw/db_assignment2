5b1153a4bb48b845133d59b19d131383f39e6b1e
reesee232 <reese.yuxin@gmail.com>
2025-08-11 16:41:36 +0800

    editted q3

--- q3.py
+++ q3.py
@@ -15,18 +15,20 @@ df = spark.read.option("header", True).csv(f"hdfs://{hdfs_nn}:9000/assignment2/p
 
 reviews_zip = df.select(
     sf.col("ID_TA"),
-    sf.arrays_zip(
-        sf.col("Reviews").getItem(0).alias("rtext"),
-        sf.col("Reviews").getItem(1).alias("rdate")
-    ).alias("reviews")
+    sf.explode(
+        sf.arrays_zip(
+            sf.col("Reviews").getItem(0).alias("rtext"),
+            sf.col("Reviews").getItem(1).alias("rdate")
+        ).alias("reviews")
+    )
 )
 
-reviews_exploded = reviews_zip.select(
-    sf.col("ID_TA"),
-    sf.explode("reviews")
-)
+# reviews_exploded = reviews_zip.select(
+#     sf.col("ID_TA"),
+#     sf.explode("reviews")
+# )
 
-result = reviews_exploded.select(
+result = reviews_zip.select(
     sf.col("ID_TA"),
     sf.col("reviews.rtext").alias("review"),
     sf.col("reviews.rdate").alias("date")
